31e5b88b485a6b3abbc7c3be339484268dcc89f3
karimKandil0 <kimo989yt@gmail.com>
2026-03-11 20:16:31 +0200

    feat: built setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+set -Eeuo pipefall
+
+echo ""
+echo "This scripts may request sudo privileges in order to:"
+echo " - Install Docker if its missing."
+echo " - Install gettext (envsubst)."
+echo " - Create data directories with correct permissions."
+echo ""
+echo "You can review this scripts before running it."
+echo ""
+echo "Requesting sudo access to initialize StarterLab..."
+sudo -v
+echo ""
+echo ""
+echo ""
+echo "StarterLab Setup"
+echo "================"
+echo ""
+
+# Detect OS
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Cannot detect operating system."
+    exit 1
+fi
+
+echo "Detected OS: $DISTRO"
+
+# Install dependencies
+
+echo ""
+echo "Checking dependencies..."
+
+INSTALL_DOCKER=false
+INSTALL_GETTEXT=false
+
+if ! command -v docker &>/dev/null; then
+    INSTALL_DOCKER=true
+fi
+
+if ! command -v envsubst &>/dev/null; then
+    INSTALL_GETTEXT=true
+fi
+
+if $INSTALL_DOCKER || $INSTALL_GETTEXT; then
+    echo " Installing missing dependencies..."
+fi
+
+case "$DISTRO" in
+    ubuntu|debian)
+        sudo apt update
+	$INSTALL_DOCKER && sudo apt intall -y docker.io docker-compose-plugin
+	$INSTALL_GETTEXT && sudo apt install -y gettext
+        ;;
+    arch)
+        $INSTALL_DOCKER && sudo pacman -Sy --noconfirm docker docker-compose-plugin
+	$INSTALL_GETTEXT && sudo pacman -Sy --noconfirm gettext
+	;;
+    fedora)
+        $INSTALL_DOCKER && sudo dnf install -y docker docker-compose-plugin
+	&INSTALL_GETTEXT && sudo dnf install -y gettext
+    *)
+        echo "Unsupported distro: $DISTRO"
+	exit 1
+	;;
+esac
+
+if ! systemctl is-active --quiet docker; then 
+    echo "Starting Docker..."
+    sudo systemctl enable docker
+    sudo systemctl start docker
+fi
+
+# Ask user configuration
+
+echo ""
+echo "Configuration"
+echo "============="
+
+read -rp "Port for StarterLab [8080]:" PORT
+if ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
+    echo "Invalid port."
+    exit 1
+fi
+PORT=${PORT:-8080}
+
+read -rp "Enable Vaultwarden? (Y/n):" ENABLE_VAULT
+ENABLE_VAULT=${ENABLE_VAULT:-Y}
+
+read -rp "Enable Gitea? (Y/n):" ENABLE_GITEA
+ENABLE_GITEA=${ENABLE_GITEA:-Y}
+
+read -rp "Enable Grafana? (Y/n):" ENABLE_Grafana
+ENABLE_GRAFANA=${ENABLE_GRAFANA:-Y}
+
+# Create .env
+
+echo ""
+echo "Generating environment configuration"
+
+if [ -f ".env" ]; then
+    echo ".env already exists in project root. Skipping creation"
+else
+    echo "Writing configuration to .env"
+
+    cat > .env <<EOF
+PORT=$PORT
+
+HOME_HOST=home.localhost
+VAULT_HOST=vault.localhost
+GITEA_HOST=gitea.localhost
+GRAFANA_HOST=grafana.localhost
+
+ENABLE_VAULTWARDEN=$ENABLE_VAULT
+ENABLE_GITEA=$ENABLE_GITEA
+ENABLE_GRAFANA=$ENABLE_GRAFANA
+EOF
+fi
+
+
+
+# Create data directories
+
+echo ""
+echo "Creating data directories"
+
+mkdir -p data/gitea
+mkdir -p data/grafana
+mkdir -p data/vaultwarden
+
+# Set permissions
+
+echo ""
+echo "Setting container permissions..."
+
+sudo chown -R 1000:1000 data/vaultwarden || true
+sudo chown -R 1000:1000 data/gitea || true
+sudo chown -R 472:472 data/grafana || true
+sudo usermod -aG docker $USER
+
+echo ""
+echo "You may need to log out and back in for Docker permissions"
+
+# Finish
+
+echo ""
+echo "Setup complete!"
+echo ""
+echo "Start StarterLab with:"
+echo ""
+echo "./scripts/start.sh"
+echo ""
